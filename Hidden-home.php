128efd396681a7200517e6c75525f429bc393666
Peter Gallwas <peter.gallwas@outlook.com>
2020-09-20 23:16:26 +0000

    Added Hidden-home.php

---
+++ Hidden-home.php
@@ -0,0 +1,38 @@
+<html>
+<link rel="stylesheet" type="text/css" href="CSS/styles.css" />
+<div class="wrapper">
+  <header role="banner">
+    <nav role="navigation">
+      <p>
+        <a href="index.php">
+          <img src="/Images/small-logo2.png" />
+        </a>
+      </p>
+
+      <link rel="stylesheet" type="text/css" href="CSS/Topnav.css" />
+      <div class="topnav">
+        <a class="active" href="index.php">Home</a>
+        <a href="Contact.php">Contact</a>
+        <a href="About-us.php">About </a> <div class="dropdown">
+        <button class="dropbtn">My youtube
+     
+            <i class="fa fa-caret-down"></i>
+          </button>
+          <dindexiv class="dropdown-content">
+            <a href="Just-a-test.php">My youtube </a>
+            <a href="dad.php">Hidden site 2</a>
+            <a href="Hidden-test">Hidden site 1</a>
+          </dindexiv>
+        </div>
+        <div class="dropdown">
+        <button class="dropbtn">Other
+     
+            <i class="fa fa-caret-down"></i>
+          </button>
+          <dindexiv class="dropdown-content">
+            <a href="Test-hide.php">Hidden site 1</a>
+            <a href="dad.php">Hidden site 2</a>
+            <a href="Where-are-we.php">Where are we?</a>
+          </dindexiv>
+        </div>
+    </nav>
