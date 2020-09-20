2cb601c84b077459959d5f75ee61379c147cdfc8
peter.gallwas <peter.gallwas@outlook.com>
2020-09-20 16:52:35 +1200

    Code chang

--- Just-a-test.php
+++ Just-a-test.php
@@ -1,5 +1,13 @@
 <html>
 <link rel="stylesheet" type="text/css" href="CSS/styles.css" />
+
+
+<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+<link rel="manifest" href="/site.webmanifest">
+
+
 <div class="line"></div>
 <div class="wrapper">
   <header role="banner">
--- index.php
+++ index.php
@@ -1,6 +1,65 @@
 <html>
 <link rel="stylesheet" type="text/css" href="CSS/styles.css" />
+
+<div class="line"<html>
+<link rel="stylesheet" type="text/css" href="CSS/styles.css" />
+
+
+<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+<link rel="manifest" href="/site.webmanifest">
+
+
 <div class="line"></div>
+<div class="wrapper">
+  <header role="banner">
+    <nav role="navigation">
+      <p>
+        <a href="index.php">
+          <img src="/Images/small-logo2.png" />
+        </a>
+      </p>
+
+      <link rel="stylesheet" type="text/css" href="CSS/Topnav.css" />
+      <div class="topnav">
+        <a class="active" href="index.php">Home</a>
+        <a href="Contact.php">Contact</a>
+        <a href="About-us.php">About </a>
+        <a href="Just-a-test.php">My youtube</a>
+        <div class="dropdown">
+          <button class="dropbtn">Hidden sites
+            <i class="fa fa-caret-down"></i>
+          </button>
+          <dindexiv class="dropdown-content">
+            <a href="Test-hide.php">Hidden site 1</a>
+            <a href="dad.php">Hidden site 2</a>
+            <a href="Where-are-we.php">Where are we?</a>
+          </dindexiv>
+        </div>
+    </nav>
+    <p1>
+      <h4>
+        Pleas suscribe to my Youtube channe.This is just a plug
+      </h4>
+      </h4>
+
+
+      <script src="https://apis.google.com/js/platform.js"></script>
+
+      <div class="g-ytsubscribe" data-channelid="UCR_nyRN56CuYDZ8P2YeX8FQ" data-layout="full" data-theme="dark"
+        data-count="shown"></div>
+
+</html>></div>
+
+
+
+<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+<link rel="manifest" href="/site.webmanifest">
+
+
 <div class="wrapper">
   <header role="banner">
     <nav role="navigation">
