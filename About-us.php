0f9e542539db8aa86bb7a6f0a3262fddd59863d5
bZ04rGcrRlL01bqJgGWg1f7c7j3jbWAGQe4LH4mbZM2582Mkd2HneY5SDgrs <>
2020-09-21 18:47:58 +1200

    1

--- About-us.php
+++ About-us.php
@@ -1,4 +1,4 @@
-</html>
+<html>
 <link rel="stylesheet" type="text/css" href="CSS/styles.css" />
 <div class="wrapper">
   <header role="banner">
@@ -23,23 +23,7 @@
      
             <i class="fa fa-caret-down"></i>
           </button>
-          <dindexiv class="dropdown-content">
-            <a href="Just-a-test.php">My exprince at westrolleston </a>
-            <a href="">Placeholder</a>
-            <a href="">Placeholder1</a>
-          </dindexiv>
-        </div>
-        <div class="dropdown">
-        <button class="dropbtn">Other
-     
-            <i class="fa fa-caret-down"></i>
-          </button>
-          <dindexiv class="dropdown-content">
-            <a href="Test-hide.php">Hidden site 1</a>
-            <a href="dad.php">Hidden site 2</a>
-            <a href="Where-are-we.php">Where are we?</a>
-          </dindexiv>
-        </div>
+          </div>
     </nav>
   </header>
   <main role="main">
