88166aac63f7259a9608b8443c7c560201f65141
bZ04rGcrRlL01bqJgGWg1f7c7j3jbWAGQe4LH4mbZM2582Mkd2HneY5SDgrs <>
2020-09-22 13:10:10 +1200

    ADD A PAGE

--- Test-hide.php
+++ Test-hide.php
@@ -1,54 +1,44 @@
-<html>
-<link rel="stylesheet" type="text/css" href="CSS/styles.css" />
-<div class="line"></div>
-<div class="wrapper">
+<!DOCTYPE html>
+  <head>
+    <title>West Rolleston</title>
+    <link rel="stylesheet" type="text/css" href="CSS/styles.css" />
+    <link rel="stylesheet" type="text/css" href="CSS/Topnav.css" />
+  </head>
+<body>
+  <div class="wrapper">
   <header role="banner">
     <nav role="navigation">
       <p>
         <a href="index.php">
-          <img src="/Images/small-logo2.png" />
+          <img src="./Images/small-logo2.png" />
         </a>
       </p>
 
-  
       <div class="topnav">
         <a class="active" href="index.php">Home</a>
         <a href="Contact.php">Contact</a>
         <a href="About-us.php">About </a> 
         <a href="Our-story.php">Reference</a> 
         <a href="Where-are-we.php">Where are we?</a>
-        </div>
+        <a href="Test-hide.php">The ambassadors</a>
     </nav>
-
-<body>
-  <p1>
-  Welcome to the hidden site
-    This site is just for testing
-  </p1>
-</body>
-
-</html>
-
-<html>
-<link rel="stylesheet" type="text/css" href="CSS/Topnav.css" />
-<div class="topnav" id="myTopnav">
-  <a href="#home" class="active">Home</a>
-  <a href="#news">News</a>
-  <a href="#contact">Contact</a>
-  <div class="dropdown">
-     <button class="dropbtn">Other
-     
-      <i class="fa fa-caret-down"></i>
-    </button>
-    <div class="dropdown-content">
-      <a href="Test-hide.php">Hidden site 1</a>
-      <a href="dad.php">Hidden site 2</a>
-    <a href="Where are we.php">Where are we?</a>
-    </div>
-
+  </header>
+  <main role="main">
+    <section class="sec-intro" role="section">
+      <img src="./Images/image.png" alt="West Rolleston is a great school" />
+      <footer>
+        <div role="navigation">
+          <ul class="nav-ul">
   </div>
-  <a href="#about">About</a>
-  <a href="javascript:void(0);" class="icon" onclick="myFunction()">&#9776;</a>
-</div>
 
+<div class="body">
+ These are our ambassadors
+</div>
+<div class="body">
+  
+</div>
+<div class="body">
+  
+</div>
+</body>
 </html>
