648ac6f8850c9be71fef17216fded0510063dcaf
Husky <peter.gallwas@outlook.com>
2020-10-09 19:19:38 +1300

    code

--- Sineup.php
+++ Sineup.php
@@ -1,6 +1,29 @@
 <!DOCTYPE html>
 <head>
 <link rel="stylesheet" type="text/css" href="CSS/sineup.css" />
+<title>West Rolleston</title>
+    <link rel="stylesheet" type="text/css" href="CSS/styles.css" />
+    <link rel="stylesheet" type="text/css" href="CSS/Topnav.css" />
+  </head>
+<body>
+  <div class="wrapper">
+  <header role="banner">
+    <nav role="navigation">
+      <p>
+        <a href="index.php">
+          <img src="./Images/small-logo2.png" />
+        </a>
+      </p>
+
+      <div class="topnav">
+        <a class="active" href="index.php">Home</a>
+        <a href="Contact.php">Contact</a>
+        <a href="About-us.php">About </a> 
+        <a href="Our-story.php">Reference</a> 
+        <a href="Where-are-we.php">Where are we?</a>
+        <a href="Test-hide.php">The ambassadors</a>
+        <a href="Sineup.php ">Sign up</a>
+    </nav>
 </head>
 <body>
 <form action="action_page.php" style="border:1px solid #ccc">
