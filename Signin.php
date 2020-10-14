38be229a6847950c430dd1b027ed3ac59df3dc08
Peter Gallwas <peter.gallwas@outlook.com>
2020-10-15 08:46:34 +1300

    code

--- Signin.php
+++ Signin.php
@@ -34,7 +34,7 @@
     
     </header>
     <Body>
-    <form action="action_page.php" method="post">
+    <form action="sineupfinish.php" method="post">
   <div class="imgcontainer">
     <img src="img_avatar2.png" alt="Avatar" class="avatar">
   </div>
