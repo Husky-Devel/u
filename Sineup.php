a914eca9cc2b7b6e2385f1300150e83c67fee741
peter.gallwas <peter.gallwas@outlook.com>
2020-11-02 18:49:05 +1300

    g sighn in

--- Sineup.php
+++ Sineup.php
@@ -4,6 +4,8 @@
 <title>West Rolleston</title>
     <link rel="stylesheet" type="text/css" href="CSS/styles.css" />
     <link rel="stylesheet" type="text/css" href="CSS/Topnav.css" />
+    <script src="https://apis.google.com/js/platform.js" async defer></script>
+
   </head>
 <body>
   <div class="wrapper">
@@ -25,34 +27,10 @@
     </nav>
 </head>
 <body>
-<form action="action_page.php" style="border:1px solid #ccc">
-  <div class="container">
-    <h1>Sign Up</h1>
-    <p>Please fill in this form to create an account.</p>
-    <hr>
+  <meta name="google-signin-client_id" content="606387351884-maak4t0lmlpn57i8iiumkh4p0u62gtjc.apps.googleusercontent.com">
 
-    <label for="email"><b>Email</b></label>
-    <input type="text" placeholder="Enter Email" name="email" required>
+  <div class="g-signin2" data-onsuccess="onSignIn"></div>
 
-    <label for="psw"><b>Password</b></label>
-    <input type="password" placeholder="Enter Password" name="psw" required>
-
-    <label for="psw-repeat"><b>Repeat Password</b></label>
-    <input type="password" placeholder="Repeat Password" name="psw-repeat" required>
-
-    <label>
-      <input type="checkbox" checked="checked" name="remember" style="margin-bottom:15px"> Remember me
-    </label>
-
-    <p>By creating an account you agree to our <a href="#" style="color:dodgerblue">Terms & Privacy</a>.</p>
-
-    <div class="clearfix">
-      <a href="index.php">cancel</a>
-      <a href="sineupfinish.php">Sign up</a>
-
-    </div>
-  </div>
-</form> 
 </body>
 
 
