4e1f88fd8061288fec6a573f424392b10aaaf8d7
peter.gallwas <peter.gallwas@outlook.com>
2020-11-02 22:34:35 +1300

    Code

---
+++ Wrapper.php
@@ -0,0 +1,14 @@
+<html xmlns="https://docs.google.com/spreadsheets/u/1/?tgif=d">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Untitled Document</title>
+<link href="/CSS/Wrapper.css" rel="stylesheet" type="text/css" />
+</head>
+ 
+<body>
+<div id="Wrapper">
+ 
+<div id="Layer1"></div>
+</div>
+</body>
+</html>
