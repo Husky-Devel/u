4959b92a1bb42697512e57a498bb5ce2cadfa7b7
peter <peter.gallwas@outlook.com>
2020-11-09 15:29:14 +1300

    Code

--- Wrapper.php
+++ Wrapper.php
@@ -1,14 +1,25 @@
-<html xmlns="https://youtube.com">
+<!DOCTYPE html>
+    
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-<title>Untitled Document</title>
-<link href="/CSS/Wrapper.css" rel="stylesheet" type="text/css" />
-</head>
- 
 <body>
-<div id="Wrapper">
- 
-<div id="Layer1"></div>
-</div>
-</body>
+  <div class="wrapper">
+  <header role="banner">
+    <nav role="navigation">
+      <p>
+        <a href="index.php">
+          <img src="./Images/small-logo2.png" />
+        </a>
+      </p>
+
+      <div class="topnav">
+        <a class="active" href="index.php">Home</a>
+        <a href="Contact.php">Contact</a>
+        <a href="About-us.php">About </a> 
+        <a href="Our-story.php">Reference</a> 
+        <a href="Where-are-we.php">Where are we?</a>
+        <a href="Sineup.php ">Sign up</a>
+    </nav>
+    <link href="/CSS/Wrapper.css" rel="stylesheet" type="text/css" />
+    <iframe src="URL"></iframe>
+</head>
 </html>
