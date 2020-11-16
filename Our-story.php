eb5ac6d398b2c03b172a823367ce07960bb3f418
peter <peter.gallwas@outlook.com>
2020-11-16 15:37:44 +1300

    Code

--- Our-story.php
+++ Our-story.php
@@ -3,36 +3,30 @@
     <title>West Rolleston</title>
     <link rel="stylesheet" type="text/css" href="CSS/styles.css" />
     <link rel="stylesheet" type="text/css" href="CSS/Topnav.css" />
-  </head>
-<body>
-  <div class="wrapper">
-  <header role="banner">
-    <nav role="navigation">
-      <p>
-        <a href="index.php">
-          <img src="./Images/small-logo2.png" />
-        </a>
-      </p>
-
-      <div class="topnav">
-        <a class="active" href="index.php">Home</a>
-        <a href="Contact.php">Contact</a>
-        <a href="About-us.php">About </a> 
-        <a href="Our-story.php">Reference</a> 
-        <a href="Where-are-we.php">Where are we?</a>
-        <a href="Test-hide.php">The ambassadors</a>
-    </nav>
-  </header>
-  <main role="main">
-    <section class="sec-intro" role="section">
-      <img src="./Images/Headerimg.jpg" alt="West Rolleston is a great school" />
-      <footer>
-        <div role="navigation">
-          <ul class="nav-ul">
-  </div>
-    </nav>
-<div>
-  Thank you to the real westrolleston site that can be found here at www.westrolleston.school.nz all the images are from there as well as the contact info.
-</div>
+    
+    <body>
+      <div class="wrapper">
+      <header role="banner">
+        <nav role="navigation">
+          <p>
+            <a href="index.php">
+              <img src="./Images/small-logo2.png" />
+            </a>
+          </p>
+    
+          <div class="topnav">
+            <a class="active" href="index.php">Home</a>
+            <a href="Contact.php">Contact</a>
+            <a href="About-us.php">About </a> 
+            <a href="Our-story.php">Reference</a> 
+            <a href="Where-are-we.php">Where are we?</a>
+            <a href="Sineup.php ">Sign up</a>
+        </nav>
+        <link href="/CSS/Wrapper.css" rel="stylesheet" type="text/css" />
+        <iframe src="https://westrolleston.school.nz" style="border:0px #ffffff none;" name="myiFrame" scrolling="yes" frameborder="0" marginheight="0px" marginwidth="0px" height="900px" width="1000px" allowfullscreen></iframe>
+    </head>
+    
+    <body></body>
+    </html>
 
 </html>
