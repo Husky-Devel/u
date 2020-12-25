0c2422ce6bc5c8c6c75e3fc04bd8db128a4a717b
peter <peter.gallwas@outlook.com>
2020-12-26 10:43:23 +1300

    Hidding

---
+++ Dev-Home.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+  <head>
+    <title>West Rolleston</title>
+    <link rel="stylesheet" type="text/css" href="CSS/styles.css" />
+    <link rel="stylesheet" type="text/css" href="CSS/Topnav.css" />
+  </head>
+<body>
+  <div class="wrapper">
+  <header role="banner">
+    <nav role="navigation">
+      <p>
+        <a href="index.php">
+          <img src="./Images/small-logo2.png" />
+        </a>
+      </p>
+
+      <div class="topnav">
+        <a class="active" href="index.php">Home</a>
+        <a href="Contact.php">Contact</a>
+        <a href="About-us.php">About </a> 
+        <a href="Our-story.php">Reference</a> 
+        <a href="Where-are-we.php">Where are we?</a>
+        <a href="Indev.php">Sign up</a>
+        
+    </nav>
+  </header>
+  <main role="main">
+    <section class="sec-intro" role="section">
+      <img src="./Images/Headerimg.jpg" alt="West Rolleston is a great school" />
+      <footer>
+        <div role="navigation">
+          <ul class="nav-ul">
+  </div>
+  
+<div class="topnav">
+  <a class="active" href="#home">Home</a>
+  <a href="#about">About</a>
+  <a href="#contact">Contact</a>
+  <div class="search-container">
+    <form action="/action_page.php">
+      <input type="text" placeholder="Search.." name="search">
+      <button type="submit">Submit</button>
+    </form>
+  </div>
+</div>
+
+<div class="body">
+ Welcome to my redesign of the school website click on one of the links above.
+</div>
+<div class="body">
+  
+</div>
+<div class="body">
+  
+</div>
+</body>
+</html>
+
+    
+
