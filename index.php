8fcb8b4eef75cbbb49b57bc994f0a49bb83f4585
peter <peter.gallwas@outlook.com>
2021-01-19 11:08:11 +1300

    Mass change

--- index.php
+++ index.php
@@ -36,7 +36,7 @@ body {
   </header>
   <main role="main">
     <section class="sec-intro" role="section">
-      <img src="./Images/Headerimg.jpg" alt="West Rolleston is a great school but this img could not be found" />
+      <img src="" alt="West Rolleston is a great school but this img could not be found" />
       <footer>
         <div role="navigation">
           <ul class="nav-ul">
@@ -56,10 +56,322 @@ body {
         </nav>
         <p  Made by Husky,hosting by Microsoft azure      </p>
       </footer>
-      
+
+
+ 
+	<div class="container">
+		<div class="row">
+	
+			<!-- LOGO -->
+			<div class="col-xs-8 col-sm-6 col-md-3 logo">
+				<div class="logo-image logo-control">
+					<a href="/" title="West Rolleston Primary School">
+													<img class="logo-img" src="/images/logo.png" alt="West Rolleston Primary School">
+																			<img class="logo-img-sm" src="/images/small-logo.png" alt="West Rolleston Primary School">
+												<span>West Rolleston Primary School</span>
+					</a>
+					<small class="site-slogan"></small>
+				</div>
+			</div>
+			<!-- //LOGO -->
+			
+							<div class="head-right">
+											<!-- HEAD SEARCH -->
+						<div class="head-search  hidden-sm hidden-xs">
+							<div class="dropdown">
+							  <button class="btn btn-primary dropdown-toggle sr-only" type="button" id="head-search" data-toggle="dropdown">
+							    <i class="fa fa-search"></i>
+							  </button>
+							  <div class="dropdown-menu" role="menu" aria-labelledby="head-search">
+							    <div class="search">
+	<form action="/" method="post" class="form-inline form-search">
+		<label for="mod-search-searchword" class="element-invisible">Search</label> <input name="searchword" id="mod-search-searchword" maxlength="200" class="form-control search-query" type="search" size="30" placeholder="Search">		<input type="hidden" name="task" value="search">
+		<input type="hidden" name="option" value="com_search">
+		<input type="hidden" name="Itemid" value="435">
+	</form>
+</div>
+
+							  </div>
+							</div>
+						</div>
+						<!-- //HEAD SEARCH -->
+						
+										
+					<!-- Brand and toggle get grouped for better mobile display -->
+					<div class="navbar-header">
+					
+									
+													
+<button class="btn btn-primary off-canvas-toggle  hidden-lg hidden-md" type="button" data-pos="right" data-nav="#t3-off-canvas" data-effect="off-canvas-effect-1">
+  <i class="fa fa-bars"></i>
+</button>
+
+<!-- OFF-CANVAS SIDEBAR -->
+
+<!-- //OFF-CANVAS SIDEBAR -->
+									
+					</div>
+				</div>
+			
+			<!-- MAIN NAVIGATION -->
+			<nav id="t3-mainnav" class="col-sm-6 col-md-9 pull-right navbar navbar-default t3-mainnav">
+			
+								
+					<div class="t3-navbar navbar-collapse collapse">
+						<div class="t3-megamenu animate elastic" data-duration="400" data-responsive="true">
+<ul class="nav navbar-nav level0">
+<li class="dropdown mega" data-id="532" data-level="1">
+<a class=" dropdown-toggle" href="#" data-target="#" data-toggle="dropdown">Vision &amp; Values<em class="caret"></em></a>
+
+<div class="nav-child dropdown-menu mega-dropdown-menu" style="width: 220px;" data-width="220"><div class="mega-dropdown-inner">
+<div class="row">
+<div class="col-xs-12 mega-col-nav" data-width="12"><div class="mega-inner">
+<ul class="mega-nav level1">
+<li data-id="579" data-level="2">
+<a class="" href="/our-vision-and-values/our-journey" data-target="#">Our Journey </a>
+
+</li>
+<li data-id="582" data-level="2">
+<a class="" href="/our-vision-and-values/our-vision" data-target="#">Our Vision </a>
+
+</li>
+<li data-id="583" data-level="2">
+<a class="" href="/our-vision-and-values/our-emerging-values" data-target="#">Our Emerging Values </a>
+
+</li>
+<li data-id="642" data-level="2">
+<a class="" href="/our-vision-and-values/strategic-plan-and-annual-report" data-target="#">Strategic Plan and Annual Report </a>
+
+</li>
+</ul>
+</div></div>
+</div>
+</div></div>
+</li>
+<li class="dropdown mega" data-id="585" data-level="1">
+<a class=" dropdown-toggle" href="#" data-target="#" data-toggle="dropdown">School Community<em class="caret"></em></a>
+
+<div class="nav-child dropdown-menu mega-dropdown-menu" style="width: 230px" data-width="230"><div class="mega-dropdown-inner">
+<div class="row">
+<div class="col-xs-12 mega-col-nav" data-width="12"><div class="mega-inner">
+<ul class="mega-nav level1">
+<li data-id="616" data-level="2">
+<a class="" href="/school-community/staff-vacancies" data-target="#">Staff Vacancies </a>
+
+</li>
+<li data-id="584" data-level="2">
+<a class="" href="/school-community/our-people" data-target="#">Our People </a>
+
+</li>
+<li data-id="606" data-level="2">
+<a class="" href="/school-community/school-map" data-target="#">School Map </a>
+
+</li>
+<li data-id="586" data-level="2">
+<a class="" href="/school-community/establishment-bot" data-target="#">Elected BoT </a>
+
+</li>
+<li class="dropdown-submenu mega" data-id="587" data-level="2">
+<a class="" href="/school-community/grow-wrps" data-target="#">GROW WRPS   . </a>
+
+<div class="nav-child dropdown-menu mega-dropdown-menu"><div class="mega-dropdown-inner">
+<div class="row">
+<div class="col-xs-12 mega-col-nav" data-width="12"><div class="mega-inner">
+<ul class="mega-nav level2">
+<li data-id="625" data-level="3">
+<a class="" href="/school-community/grow-wrps/grow-wrps-events" data-target="#">Events </a>
+
+</li>
+<li data-id="626" data-level="3">
+<a class="" href="/school-community/grow-wrps/fundraising" data-target="#">Fundraising </a>
+
+</li>
+<li data-id="627" data-level="3">
+<a class="" href="/school-community/grow-wrps/grow-wrps-calendar" data-target="#">Calendar </a>
+
+</li>
+</ul>
+</div></div>
+</div>
+</div></div>
+</li>
+<li data-id="588" data-level="2">
+<a class="" href="/school-community/our-library" data-target="#">Our Library </a>
+
+</li>
+<li data-id="614" data-level="2">
+<a class="" href="/school-community/school-lunches" data-target="#">School Lunches </a>
+
+</li>
+<li data-id="589" data-level="2">
+<a class="" href="/school-community/policies-and-procedures" data-target="#">Policies &amp; Procedures </a>
+
+</li>
+<li data-id="590" data-level="2">
+<a class="" href="/school-community/hire-of-facilities" data-target="#">Hire of Facilities </a>
+
+</li>
+<li data-id="628" data-level="2">
+<a class="" href="/school-community/hire-enquiry" data-target="#">Hire Enquiry </a>
+
+</li>
+<li data-id="612" data-level="2">
+<a class="" href="/school-community/waitaha-special-school" data-target="#">Waitaha Special School </a>
+
+</li>
+</ul>
+</div></div>
+</div>
+</div></div>
+</li>
+<li class="dropdown mega" data-id="600" data-level="1">
+<a class=" dropdown-toggle" href="#" target="_blank" title="Linc Ed" data-target="#" data-toggle="dropdown">Our Learning<em class="caret"></em></a>
+
+<div class="nav-child dropdown-menu mega-dropdown-menu" style="width: 250px;" data-width="250"><div class="mega-dropdown-inner">
+<div class="row">
+<div class="col-xs-12 mega-col-nav" data-width="12"><div class="mega-inner">
+<ul class="mega-nav level1">
+<li data-id="599" data-level="2">
+<a class="" href="/our-learning/caring-for-our-learners" data-target="#">Caring for our Learners </a>
+
+</li>
+<li data-id="601" data-level="2">
+<a class="" href="/our-learning/the-studios" data-target="#">The Studios </a>
+
+</li>
+<li data-id="602" data-level="2">
+<a class="" href="/our-learning/our-school-day" data-target="#">Our School Day </a>
+
+</li>
+<li data-id="603" data-level="2">
+<a class="" href="/our-learning/curriculum" data-target="#">Curriculum </a>
+
+</li>
+<li data-id="613" data-level="2">
+<a class="" href="/our-learning/the-arts" data-target="#">The Arts </a>
+
+</li>
+<li data-id="605" data-level="2">
+<a class="" href="/our-learning/stationery" data-target="#">Stationery </a>
+
+</li>
+<li data-id="604" data-level="2">
+<a class="" href="/our-learning/term-dates" data-target="#">Term Dates </a>
+
+</li>
+<li data-id="617" data-level="2">
+<a class="" href="https://westrolleston.linc-ed.com/wp-login.php?redirect_to=https://westrolleston.linc-ed.com/?p=1" data-target="#">Linc Ed</a>
+
+</li>
+<li data-id="643" data-level="2">
+<a class="" href="/our-learning/school-app" data-target="#">Our School APP </a>
+
+</li>
+<li data-id="630" data-level="2">
+<a class="" href="/our-learning/websites-for-learning" title="Websites for Learning" data-target="#">Online learning </a>
+
+</li>
+<li data-id="631" data-level="2">
+<a class="" href="/our-learning/bring-your-own-chromebook" data-target="#">Bring Your Own Chromebook </a>
+
+</li>
+<li data-id="632" data-level="2">
+<a class="" href="http://techlps.blogspot.co.nz/" data-target="#">Technology at Lincoln</a>
+
+</li>
+<li data-id="633" data-level="2">
+<a class="" href="/our-learning/our-values-leaders" data-target="#">Our Values Leaders </a>
+
+</li>
+</ul>
+</div></div>
+</div>
+</div></div>
+</li>
+<li class="dropdown mega" data-id="581" data-level="1">
+<a class=" dropdown-toggle" href="#" data-target="#" data-toggle="dropdown">News<em class="caret"></em></a>
+
+<div class="nav-child dropdown-menu mega-dropdown-menu" style="width: 240px;" data-width="240"><div class="mega-dropdown-inner">
+<div class="row">
+<div class="col-xs-12 mega-col-nav" data-width="12"><div class="mega-inner">
+<ul class="mega-nav level1">
+<li data-id="580" data-level="2">
+<a class="" href="/news/calendar" data-target="#">Calendar </a>
+
+</li>
+<li data-id="598" data-level="2">
+<a class="" href="/news/newsletters-and-notices" data-target="#">Newsletters &amp; Notices </a>
+
+</li>
+<li data-id="498" data-level="2">
+<a class="" href="/news/our-stories" data-target="#">Our Stories </a>
+
+</li>
+</ul>
+</div></div>
+</div>
+</div></div>
+</li>
+<li class="dropdown mega" data-id="592" data-level="1">
+<a class=" dropdown-toggle" href="#" data-target="#" data-toggle="dropdown">Enrolment<em class="caret"></em></a>
+
+<div class="nav-child dropdown-menu mega-dropdown-menu" style="width: 260px;" data-width="260"><div class="mega-dropdown-inner">
+<div class="row">
+<div class="col-xs-12 mega-col-nav" data-width="12"><div class="mega-inner">
+<ul class="mega-nav level1">
+<li data-id="591" data-level="2">
+<a class="" href="/enrolment/enrolment-zone" data-target="#">Enrolment Zone </a>
+
+</li>
+<li data-id="615" data-level="2">
+<a class="" href="https://enrolments.linc-ed.com/apply/NZ/584" target="_blank" data-target="#">Online Enrolment</a>
+
+</li>
+<li data-id="645" data-level="2" class="">
+<a class="" href="https://www.westrolleston.school.nz/images/enrolment/onlineenrolmentdocuments.pdf" target="_blank" data-target="#">Enrolment Documents</a>
+
+</li>
+<li data-id="593" data-level="2">
+<a class="" href="/enrolment/uniform" data-target="#">Uniform </a>
+
+</li>
+<li data-id="594" data-level="2">
+<a class="" href="/enrolment/before-and-after-school-care" data-target="#">Before &amp; After School Care </a>
+
+</li>
+<li data-id="595" data-level="2">
+<a class="" href="/enrolment/fees-and-donations" data-target="#">Fees and Donations </a>
+
+</li>
+<li data-id="596" data-level="2">
+<a class="" href="/enrolment/safe-travel-to-school" data-target="#">Safe Travel to School </a>
+
+</li>
+</ul>
+</div></div>
+</div>
+</div></div>
+</li>
+<li data-id="597" data-level="1" class="">
+<a class="" href="/contact" data-target="#">Contact </a>
+
+</li>
+</ul>
+</div>
+
+					</div>
+			
+			</nav>
+			<!-- //MAIN NAVIGATION -->
+
+	
+		</div>
+	</div>
+     
 </div>
 </body>
 </html>
+
 
     
 
